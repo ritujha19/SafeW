0de3a34af29715650b693962a7cd26eca853833d
ritu jha <ritu05491@gmail.com>
2026-09-07 20:50:14 +0530

    adding nativeWind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack, useRouter } from "expo-router";
 import { Pressable, Text } from "react-native";
 import { isLoggedIn } from "../auth";
+import "../global.css";
 
 export default function RootLayout() {
   const router = useRouter();
@@ -9,26 +10,44 @@ export default function RootLayout() {
     router.navigate(isLoggedIn ? "/profile" : "/login");
   };
 
-  return <Stack>
-    <Stack.Screen
-      name="index"
-      options={{
-        title: "SAFE-W",
-        headerRight: () => (
-          <Pressable onPress={handleProfilePress}>
-            <Text style={{ fontSize: 24, padding: 10 }}>👤</Text>
-          </Pressable>
-        )
-      }}
-    />
-    <Stack.Screen name="createAcc" options={{ title: "Create Account " }} />
-    <Stack.Screen name="emergency" options={{ title: "Emergency" }} />
-    <Stack.Screen name="location" options={{ title: "Location" }} />
-    <Stack.Screen name="login" options={{ title: "Login" }} />
-    <Stack.Screen name="trustedContact" options={{ title: "Trusted Contacts" }} />
-    <Stack.Screen name="learn/index" options={{ title: "Learn & Prepare" }} />
-    <Stack.Screen name="learn/whatCounts" options={{ title: "Know What Counts" }} />
-    <Stack.Screen name="learn/warningSigns" options={{ title: "Know the Warning Signs" }} />
-    <Stack.Screen name="learn/safetyEssentials" options={{ title: "Safety Essentials" }} />
-  </Stack>;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "SAFE-W",
+          headerRight: () => (
+            <Pressable onPress={handleProfilePress}>
+              <Text style={{ fontSize: 24, padding: 10 }}>👤</Text>
+            </Pressable>
+          ),
+        }}
+      />
+      <Stack.Screen name="createAcc" options={{ title: "Create Account " }} />
+      <Stack.Screen name="emergency" options={{ title: "Emergency" }} />
+      <Stack.Screen name="location" options={{ title: "Location" }} />
+      <Stack.Screen name="login" options={{ title: "Login" }} />
+      <Stack.Screen
+        name="trustedContact"
+        options={{ title: "Trusted Contacts" }}
+      />
+      <Stack.Screen name="learn/index" options={{ title: "Learn & Prepare" }} />
+      <Stack.Screen
+        name="learn/whatCounts"
+        options={{ title: "Know What Counts" }}
+      />
+      <Stack.Screen
+        name="learn/warningSigns"
+        options={{ title: "Know the Warning Signs" }}
+      />
+      <Stack.Screen
+        name="learn/safetyEssentials"
+        options={{ title: "Safety Essentials" }}
+      />
+      <Stack.Screen
+        name="learn/prepareYourself"
+        options={{ title: "Prepare Yourself" }}
+      />
+    </Stack>
+  );
 }
